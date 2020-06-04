5b048a356273091c81596046ef164674e9268244
npase <npase@DESKTOP-053SBUQ.san.rr.com>
2020-06-03 18:26:34 -0700

    finished module 1 checkpoint

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -4,8 +4,20 @@ public class FasterFunctions {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
 		//Your Code Here
-		
-		
+		Thread r1=new Thread(()->SlowFunctions.slowSortLargeArray());
+		Thread r2=new Thread(()->SlowFunctions.ackermann(3, 15));
+		Thread r3=new Thread(()->SlowFunctions.millionsOfSqrts());
+		r1.start();
+		r2.start();
+		r3.start();
+		try {
+			r1.join();
+			r2.join();
+			r3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
